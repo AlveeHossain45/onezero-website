ba1757622072d87c22061939f26ffab40641535d
AlveeHossain45 <alveehossain042004@gmail.com>
2025-09-23 22:05:11 +0300

    a

--- src/data/team.js
+++ src/data/team.js
@@ -22,7 +22,7 @@ export const teamData = [
     // --- পরিবর্তন: লিঙ্কডইন ও টুইটার সরানো হয়েছে ---
     social: {
       
-      email: '#', // আপনি চাইলে এখানে साहिल এর ইমেইল যোগ করতে পারেন
+      email: 'samiujjaman643@gmail.com', // আপনি চাইলে এখানে साहिल এর ইমেইল যোগ করতে পারেন
     },
     portfolio: 'https://samisahil.github.io/samiprofile/',
   }
